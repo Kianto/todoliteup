d15fe3a2700b94d10a5e2ee4bf85e697e63e93a6
kietapvn <kiet.to@andpad.co.jp>
2023-10-29 16:07:56 +0700

    support

--- todoliteup_2/lib/screens/account/controllers/account_controller.dart
+++ todoliteup_2/lib/screens/account/controllers/account_controller.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 import 'package:get/get.dart';
 import 'package:todoliteup/screens/login/login_binding.dart';
 import 'package:todoliteup/screens/login/login_page.dart';
+import 'package:todoliteup/screens/support/support_binding.dart';
+import 'package:todoliteup/screens/support/support_page.dart';
 
 class AccountController extends GetxController {
   final randNum = 20.obs;
@@ -17,5 +19,9 @@ class AccountController extends GetxController {
 
   void logout() {
     Get.offAll(() => const LoginPage(), binding: LoginBinding());
+  }
+
+  void support() {
+    Get.to(() => const SupportPage(), binding: SupportBinding());
   }
 }
