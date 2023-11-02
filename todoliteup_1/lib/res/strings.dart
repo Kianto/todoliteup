ce2320b271fc67e9141b2f0f06c5e62b314f9e1f
kietapvn <kiet.to@andpad.co.jp>
2023-11-02 12:23:22 +0700

    update

---
+++ todoliteup_1/lib/res/strings.dart
@@ -0,0 +1,26 @@
+abstract class StringRes {
+  static const appName = "Todo LiteUp";
+
+  static const ok = "OK";
+  static const close = "Đóng";
+  static const cancel = "Huỷ";
+  static const save = "Lưu";
+  static const delete = "Xoá";
+  static const edit = "Thay đổi";
+  static const update = "Cập nhật";
+  static const failed = "Thất bại";
+  static const success = "Thành công";
+  static const error = "Lỗi";
+  static const failedMsg = "Có lỗi xảy ra khi lưu dữ liệu.";
+  static const successMsg = "Thay đổi được lưu thành công!";
+
+  static const all = 'Tất cả';
+  static const doing = 'Cần làm';
+  static const done = 'Hoàn tất';
+
+  static const description = 'Mô tả';
+  static const title = 'Tiêu đề';
+  static const noData = "Không có dữ liệu";
+
+  static const errorRequired = "Trường này không được trống";
+}
