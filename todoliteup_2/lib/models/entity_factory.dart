1873843e42f7bebdbc4ed2c23c24efd082d933b4
kietapvn <kiet.to@andpad.co.jp>
2023-12-25 12:45:47 +0700

    udpate

--- todoliteup_2/lib/models/entity_factory.dart
+++ todoliteup_2/lib/models/entity_factory.dart
@@ -4,7 +4,8 @@ import 'task.dart';
 abstract class EntityFactory {
   static Entity? getEntity(Type type, Map<String, dynamic> json) {
     switch (type) {
-      case MTask: return MTask.fromJson(json);
+      case MTask:
+        return MTask.fromJson(json);
     }
     return null;
   }
